ebf17172083d59dbdda64329b8b7abb092a8b14f
yifangalter <1727047896@qq.com>
2020-05-18 00:11:31 +0800

    workupp

---
+++ js/js.js
@@ -0,0 +1,13 @@
+function requestMethod(par1,par2,par3,callback) {
+    var xhr=new XMLHttpRequest();
+    xhr.open(par1,'https://mockapi.eolinker.com/2ZhGVxjacb39010e6753bd9c02ee803e6e3bfeab6e8007c/'+par2);
+    xhr.setRequestHeader("content-type","application/x-www-form-urlencoded");
+    xhr.send(par3);
+    xhr.onreadystatechange=function () {
+        if(xhr.readyState==4&&xhr.status==200){
+            var res=JSON.parse(xhr.responseText);
+            var str=res.data;
+            callback(str);
+        }
+    }
+}
